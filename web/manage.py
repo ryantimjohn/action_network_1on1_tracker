ff4708f235e24d1e5dac20e71afa71f07b15ead8
John Corrigan <johncorrigan@Johns-MacBook-Pro.local>
2017-11-12 18:48:58 -0600

    Add dotenv support
    - Added dotenv lib to manage ENV vars for dev & deployed environments.
    - Updated README to reflect change.

--- web/manage.py
+++ web/manage.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 import os
 import sys
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 if __name__ == "__main__":
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
     try:
         from django.core.management import execute_from_command_line
