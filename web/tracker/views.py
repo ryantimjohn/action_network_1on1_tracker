0c13699d52a56a0c080d382060cd793321d32fe6
Sade <smithsade13@gmail.com>
2017-11-21 17:45:18 -0600

    api validation to form, form bug fix, added styling for serverside errors for template

--- web/tracker/views.py
+++ web/tracker/views.py
@@ -56,19 +56,22 @@ def create_one_on_one(request):
 			current_user = request.user
 			#submit one on one data to action network using Tim's script
 			an_response = an_api_wrapper.an_post(current_user.first_name, current_user.last_name, current_user.email, contact_first_name, contact_last_name, contact_email, oneonone_notes)
+			print an_response
+			if(an_response[0] == 200):
+				#checking if contact exists, if so we use the id of the contact already in the system
+				contact_search = Contact.objects.filter(email=contact_email);
+				if(contact_search.count() != 0):
+					new_contact = contact_search[0]
+				else:	
+					new_contact = Contact(first_name=contact_first_name, last_name=contact_last_name, email=contact_email)
+					new_contact.save()
 
-			#checking if contact exists, if so we use the id of the contact already in the system
-			contact_search = Contact.objects.filter(email=contact_email);
-			if(contact_search.count() != 0):
-				new_contact = contact_search[0]
-			else:	
-				new_contact = Contact(first_name=contact_first_name, last_name=contact_last_name, email=contact_email)
-				new_contact.save()
+				#record one on one in db	
+				new_oneonone = OneOnOne(user=current_user, contact = new_contact, description=oneonone_notes, form_endpoint = an_response[1], other_enpoint=an_response[2])
+				new_oneonone.save()
+				return redirect('/')
 
-			#record one on one in db	
-			new_oneonone = OneOnOne(user=current_user, contact = new_contact, description=oneonone_notes, form_endpoint = an_response[1], other_enpoint=an_response[2])
-			new_oneonone.save()
-			return redirect('/')
+			errors.append('Error occured, unable to submit one on one form')
 
 		contacts = Contact.objects.all()
 		return render(request, 'oneonone/create.html', {'errors': errors, 'form_data': request.POST, 'contacts' : contacts })
